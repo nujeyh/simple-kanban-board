6969b86b9800bb1af5da4839b41c22042c09a94c
nujeyh <dza456@naver.com>
2022-11-01 20:19:59 +0900

    ✨ Add to do category edit button

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -1,9 +1,34 @@
-import { IToDo } from "../recoilAtom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../recoilAtom";
 
-const ToDo = ({ text }: IToDo) => {
+const ToDo = ({ text, category, id }: IToDo) => {
+  const setToDo = useSetRecoilState(toDoState);
+  const onClickCategory = (newCategory: IToDo["category"]) => {
+    setToDo((prevToDoArr) => {
+      const targetIndex = prevToDoArr.findIndex((toDo) => toDo.id === id);
+      const currToDo = { text, id, category: newCategory };
+      return [
+        ...prevToDoArr.slice(0, targetIndex),
+        currToDo,
+        ...prevToDoArr.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <>
-      <li>{text}</li>
+      <li>
+        <span>{text}</span>
+        {category !== "TODO" && (
+          <button onClick={() => onClickCategory("TODO")}>To Do</button>
+        )}
+        {category !== "DOING" && (
+          <button onClick={() => onClickCategory("DOING")}>Doing</button>
+        )}
+        {category !== "DONE" && (
+          <button onClick={() => onClickCategory("DONE")}>Done</button>
+        )}
+      </li>
     </>
   );
 };

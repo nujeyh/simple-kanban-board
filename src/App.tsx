1eef00a7aac3df5438920cc1e05d3a42818876b6
nujeyh <dza456@naver.com>
2022-11-02 18:19:46 +0900

    💄 Add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ToDoList from "./components/ToDoList";
+import { GlobalStyle } from "./styles/GlobalStyle";
 import { Reset } from "./styles/Reset";
 
 function App() {
   return (
     <>
       <Reset />
+      <GlobalStyle />
       <ToDoList />
     </>
   );

ef2d0fea1538a62d956095247ea582d18e144859
nujeyh <dza456@naver.com>
2022-11-04 19:05:44 +0900

    💄 Add `background-color` when dragging

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,7 +3,6 @@ import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import { IToDo } from "../recoilAtom";
 import ToDoCard from "./ToDoCard";
-
 interface IBoardProps {
   boardArr: { id: number; text: string }[];
   boardId: string;
@@ -11,23 +10,46 @@ interface IBoardProps {
 
 const Board = ({ boardArr, boardId }: IBoardProps) => {
   return (
-    <Droppable droppableId={boardId}>
-      {(provide) => (
-        <Wrapper ref={provide.innerRef} {...provide.droppableProps}>
-          <div>{boardId}</div>
-          {boardArr.map((toDo, index) => {
-            return <ToDoCard key={toDo.id} index={index} toDo={toDo} />;
-          })}
-          {provide.placeholder}
-        </Wrapper>
-      )}
-    </Droppable>
+    <Wrapper>
+      <div>{boardId}</div>
+      <Droppable droppableId={boardId}>
+        {(provide, snapshot) => (
+          <DroppableArea
+            ref={provide.innerRef}
+            {...provide.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+            draggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
+          >
+            {boardArr.map((toDo, index) => {
+              return <ToDoCard key={toDo.id} index={index} toDo={toDo} />;
+            })}
+            {provide.placeholder}
+          </DroppableArea>
+        )}
+      </Droppable>
+    </Wrapper>
   );
 };
-const Wrapper = styled.ul`
+
+interface IAreaProps {
+  draggingFromThisWith: boolean;
+  isDraggingOver: boolean;
+}
+
+const Wrapper = styled.div`
   background-color: ${(props) => props.theme.colors.board};
   border-radius: ${(props) => props.theme.borderRadius};
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  height: fit-content;
+  min-height: 50px;
+`;
+const DroppableArea = styled.ul<IAreaProps>`
+  flex-grow: 1;
+  transition: background-color 0.2s ease-in-out;
+  background-color: ${(props) =>
+    props.isDraggingOver ? "pink" : props.draggingFromThisWith && "green"};
 `;
 
 export default Board;

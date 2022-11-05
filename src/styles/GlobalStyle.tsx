ef72f34eabc1b36452ecd6f1839cf1ab73dc8cd6
nujeyh <dza456@naver.com>
2022-11-05 20:30:39 +0900

    💄 Change font

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,14 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    @font-face {
-      font-family: 'LeeSeoyun';
-      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-    };
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
   body {
     background-color: ${(props) => props.theme.colors.backgroundColor};
-    font-family: "LeeSeoyun";
+    font-family: "Pretendard-Regular";
   }
 `;

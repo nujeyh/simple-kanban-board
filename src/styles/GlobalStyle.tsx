94bcf11149e5c59f8513a8f854cf78d38065d7bc
nujeyh <dza456@naver.com>
2022-11-05 16:44:48 +0900

    💄 Add new font

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+      font-family: 'LeeSeoyun';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+    };
   body {
     background-color: ${(props) => props.theme.colors.backgroundColor};
+    font-family: "LeeSeoyun";
   }
 `;

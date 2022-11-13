019b06281ffc290007c23d4fe1868245b0a5382b
nujeyh <dza456@naver.com>
2022-11-14 04:56:35 +0900

    💄 Add box shadow to theme

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -2,14 +2,15 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
+    boxShadow: {
+      [key: string]: string;
+    };
     borderRadius: string;
 
     colors: {
       backgroundColor: string;
       board: string;
-      card: {
-        [key: number]: string;
-      };
+      card: string;
     };
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,16 +1,16 @@
 import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
+  boxShadow: {
+    normal: "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.20) 0px 1px 2px",
+    extend:
+      "rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
+  },
   borderRadius: "7px",
 
   colors: {
-    backgroundColor: "#ecebeb",
+    backgroundColor: "#f0f2f8",
     board: "white",
-    card: {
-      0: "#fff",
-      1: "#fff",
-      2: "#fff",
-      3: "#fff",
-    },
+    card: "white",
   },
 };
